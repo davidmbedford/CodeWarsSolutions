fbcb7974908e46c5dcf974bcf6e0f93ca86b8f02
davidmbedford <david.m.bedford1@gmail.com>
2020-08-15 14:43:15 -0400

    8kyu added

---
+++ 8kyu/parseF.js
@@ -0,0 +1,12 @@
+// Parse Float
+
+// Write function parseF which takes an input and returns a number or null 
+//if conversion is not possible. The input can be one of many different types so be aware.
+
+const parseF = s => { 
+  if (isNaN(parseFloat(s))) {
+    return null 
+  } else {
+    return parseFloat(s)
+  }
+}
