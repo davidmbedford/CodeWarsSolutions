84aa8a9b08062a365e6624109da043c8533ee7c1
davidmbedford <david.m.bedford1@gmail.com>
2020-09-03 13:12:09 -0400

    7kyu php added

---
+++ php/8kyu/areaOfAnArrow.php
@@ -0,0 +1,12 @@
+<!-- An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and 
+the centre of the rectangle. 
+
+a and b are integers and > 0
+
+Write a function which returns the area of the arrow. -->
+
+<?php 
+function arrow_area(int $a, int $b): float {
+  return $a*($b/2)/2;
+}
+?>
