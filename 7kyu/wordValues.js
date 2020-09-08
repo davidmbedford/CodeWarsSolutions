47bc14ee770d846d22ead3e147d4e95ea0d5e029
davidmbedford <david.m.bedford1@gmail.com>
2020-09-08 12:10:19 -0400

    7kyu js added

---
+++ 7kyu/wordValues.js
@@ -0,0 +1,33 @@
+// Given a string "abc" and assuming that each letter in the string has a value equal to its position in the alphabet, 
+// our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ....z = 26.
+
+// You will be given a list of strings and your task will be to return the values of the strings as explained above 
+// multiplied by the position of that string in the list. For our purpose, position begins with 1.
+
+// nameValue ["abc","abc abc"] should return [6,24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
+
+// "abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied by 1 while 
+// the value at position 2 is multiplied by 2.
+
+// Input will only contain lowercase characters and spaces.
+
+// Good luck!
+
+const wordValue = (a) => {
+  let ans = []
+
+  for (let i in a) {
+    let wordValue = 0
+    let word = a[i]
+    
+    for (let z in word) {
+      if (word[z] !== " ") {
+        let charVal = parseInt(word[z], 36) -9;
+        wordValue += charVal
+      }
+    }
+    
+    ans.push(wordValue*(parseInt(i)+1));
+  }
+  return ans
+}
